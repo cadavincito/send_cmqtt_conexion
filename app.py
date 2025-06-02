cf47f7a65586e4852dbb94d8c7bec982f7a55796
Cadavinci <cadavincinfo@gmail.com>
2025-06-01 20:54:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -106,20 +106,6 @@ if st.button('Apagar Luz Externa'):
 else:
     st.write('')
 
-values = st.slider('Selecciona el rango de valores',0.0, 100.0)
-st.write('Values:', values)
-
-if st.button('Enviar valor analógico'):
-    client1= paho.Client("GIT-HUB")                           
-    client1.on_publish = on_publish                          
-    client1.connect(broker,port)   
-    message =json.dumps({"Analog": float(values)})
-    ret= client1.publish("cmqtt_a", message)
-    
- 
-else:
-    st.write('')
-
 
 
 
